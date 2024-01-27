fb9a4db06dd8adfc27a36117b1cdf3e56e431031
yimd85 <yimd85@gmail.com>
2024-01-27 15:24:32 -0500

    initial

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+//when we need state, we need to import in the useState hook
+//(applies to function components only. 
+//look into class components for more detail wont be covering class components today)
+import { useState } from 'react';
 
-function App() {
+const App = () => {
+  const [count, setCount] = useState(0)
+  //"count" is a variable. this is value of state
+  //the initial value of 'count' is 200 because that is value inside the useState() on the line above
+
+  //the "setCount" is a function that is used to update the "count" state variable
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div >
+      <p><b>current count: </b>{count}</p>
+      <button
+        onClick={() => {
+          setCount(count + 1)
+        }}
+      >
+        +
+      </button>
+      <button
+        onClick={() => {
+          setCount(count - 1)
+        }}
+      >
+        -
+      </button>
+      <button
+        onClick={() => {
+          setCount(100)
+        }}
+      >
+        set to 100
+      </button>
+      <button
+        onClick={() => {
+          setCount(0)
+        }}
+      >
+        reset to 0
+      </button>
     </div>
   );
 }
